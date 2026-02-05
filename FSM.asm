6ee87176ec03746dceb2fb3eda1a8e2be8f7be69
Krish-2006 <141599075+Krish-2006@users.noreply.github.com>
2026-02-04 22:01:30 -0800

    Refactor FSM logic and update temperature parameters

--- FSM.asm
+++ FSM.asm
@@ -15,24 +15,28 @@ time_reflow:   ds 1
 
 pwm_duty:      ds 1
 pwm_cnt:       ds 1
+timer_tick:    ds 1
 
 CSEG
 main:
     mov SP, #60h
-    clr KEY.1
+    setb KEY_1       
     mov P1MOD, #01h
     clr P1.0
     mov LEDRA, #00h
+    mov LEDRB, #00h
 
-    mov temp_soak,   #50
-    mov time_soak,   #20
-    mov temp_reflow, #80
-    mov time_reflow, #15
+    mov temp_soak,   #150  
+    mov time_soak,   #60   
+    mov temp_reflow, #220  
+    mov time_reflow, #45   
 
     mov fsm_state, #0
     mov seconds,   #0
     mov pwm_duty,  #0
     mov pwm_cnt,   #0
+    mov timer_tick, #0
+    mov temp_current, #0
 
     mov TMOD, #01h
     mov TH0, #0B0h
@@ -41,123 +45,135 @@ main:
 
 main_loop:
     lcall Update_Seconds
-    lcall Read_Temperature
+    lcall Read_Temperature  
     lcall FSM_Reflow
     mov LEDRA, fsm_state
     lcall PWM_Update
     sjmp main_loop
-
-Abort_Check:
-    mov a, fsm_state
-    jz abort_done
-    mov a, seconds
-    clr c
-    subb a, #60
-    jc abort_done
-    mov a, temp_current
-    clr c
-    subb a, #50
-    jnc abort_done
-    mov pwm_duty, #0
-    mov fsm_state, #0
-abort_done:
-    ret
-
+    
 FSM_Reflow:
-    lcall Abort_Check
     mov a, fsm_state
 
-FSM_STATE_0:
+FSM_STATE_0: 
     cjne a, #0, FSM_STATE_1
     mov pwm_duty, #0
-    jb  KEY.1, idle_done
-    jnb KEY.1, $
+    jb  KEY_1, idle_done
+    jnb KEY_1, $           
     mov seconds,   #0
     mov fsm_state, #1
 idle_done:
     ret
 
-FSM_STATE_1:
+FSM_STATE_1: 
     cjne a, #1, FSM_STATE_2
     mov pwm_duty, #100
     mov a, temp_soak
     clr c
     subb a, temp_current
-    jnc preheat_done
+    jnc preheat_done       
     mov seconds,   #0
     mov fsm_state, #2
 preheat_done:
     ret
 
-FSM_STATE_2:
+FSM_STATE_2: 
     cjne a, #2, FSM_STATE_3
     mov pwm_duty, #20
     mov a, seconds
     clr c
     subb a, time_soak
-    jc soak_done
+    jc soak_done           
     mov seconds,   #0
     mov fsm_state, #3
 soak_done:
     ret
 
-FSM_STATE_3:
+FSM_STATE_3: 
     cjne a, #3, FSM_STATE_4
     mov pwm_duty, #100
     mov a, temp_reflow
     clr c
     subb a, temp_current
-    jnc ramp_done
+    jnc ramp_done         
     mov seconds,   #0
     mov fsm_state, #4
 ramp_done:
     ret
 
-FSM_STATE_4:
+FSM_STATE_4: 
     cjne a, #4, FSM_STATE_5
     mov pwm_duty, #20
     mov a, seconds
     clr c
     subb a, time_reflow
-    jc reflow_done
+    jc reflow_done        
     mov pwm_duty, #0
     mov fsm_state, #5
 reflow_done:
     ret
 
-FSM_STATE_5:
+FSM_STATE_5: 
     cjne a, #5, FSM_DONE
     mov pwm_duty, #0
-    mov a, #30
+    mov a, temp_current
     clr c
-    subb a, temp_current
-    jc cooling_done
-    mov fsm_state, #0
+    subb a, #60
+    jnc cooling_done       
+    mov fsm_state, #0     
 cooling_done:
     ret
 
 FSM_DONE:
     ret
-
 Update_Seconds:
     jnb TF0, us_done
     clr TF0
     mov TH0, #0B0h
     mov TL0, #00h
+    mov a, timer_tick
+    anl a, #03h        
+    jnz tick_timer_only 
+
+    
+    mov a, fsm_state
+    
+  
+    cjne a, #0, check_heat
+    mov temp_current, #0 
+    sjmp tick_timer_only
+
+check_heat:
+    cjne a, #1, check_ramp3
+    sjmp do_heat
+check_ramp3:
+    cjne a, #3, check_cool
+    sjmp do_heat
+
+check_cool:
+    cjne a, #5, tick_timer_only
+    mov a, temp_current
+    jz tick_timer_only
+    dec temp_current     
+    dec temp_current     
+    sjmp tick_timer_only
+
+do_heat:
+    mov a, temp_current
+    cjne a, #240, inc_temp
+    sjmp tick_timer_only
+inc_temp:
+    inc temp_current
+
+tick_timer_only:
+    inc timer_tick
+    mov a, timer_tick
+    cjne a, #20, us_done 
+    mov timer_tick, #0
     inc seconds
 us_done:
     ret
 
 Read_Temperature:
-    mov ADC_C, #080h
-    nop
-    nop
-    mov ADC_C, #000h
-adc_wait:
-    mov a, ADC_C
-    jb acc.7, adc_wait
-    mov a, ADC_H
-    mov temp_current, a
     ret
 
 PWM_Update:
@@ -175,3 +191,4 @@ pwm_chk:
 pwm_on:
     setb P1.0
     ret
+END
